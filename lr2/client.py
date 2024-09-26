2a280ddb06d8cc558ba1c5ebeadd5a19509cd0cf
Bair Dobylov <krumane@mail.ru>
2024-09-26 14:25:31 +0300

    add lab2.2

--- lr2/client.py
+++ lr2/client.py
@@ -1,33 +1,16 @@
 import datetime
-import logging
 import socket
 import time
 
 from settings import HOST, PORT
 
-logging.basicConfig(
-    filename='client_conn.log',
-    filemode='w',
-    format='%(asctime)s.%(msecs)03d - %(message)s',
-    level=logging.INFO,
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
-logger = logging.getLogger(__name__)
-console_handler = logging.StreamHandler()
-logger.addHandler(console_handler)
-
 TIMEOUT = 2
-MESSAGE = b"Bair Dobylov M3O-207M"
+MESSAGE = b"some message"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    logger.info(f"Connected to {HOST}:{PORT}")
-
-    time.sleep(TIMEOUT)
-
     now = datetime.datetime.now()
-    logger.info(f"Sending MESSAGE = {MESSAGE}")
     s.sendall(MESSAGE)
-
-    data = s.recv(1024)
-    logger.info(f"Received MESSAGE = {data!r}")
+    data = s.recv(2048)
+    time.sleep(TIMEOUT)
+    print(f"Received message = {data!r}")
--- lr2/server.py
+++ lr2/server.py
@@ -1,15 +1,36 @@
+import logging
 import socket
+import time
 
 from settings import HOST, PORT
 
+logging.basicConfig(
+    filename='server_conn.log',
+    filemode='w',
+    format='%(asctime)s.%(msecs)03d - %(message)s',
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
+
+TIMEOUT = 1
+
+logger.info("Server started")
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f"Connected by {addr}")
+        logger.info(f"Connected by {addr}")
         while True:
-            data = conn.recv(1024)
+            data = conn.recv(2048)
+            logger.info(f"Received data: {data}")
             if not data:
                 break
-            conn.sendall(data)
+            time.sleep(TIMEOUT)
+            message = f"Server created by Bair Dobylov M3O-207M. {data.decode()[::-1]}"
+            conn.sendall(str.encode(message))
+            logger.info(f"Sent data: {message}")
+        logger.info("Client disconnected")
